40852933b7b9682881f0b5fb3725e914b4e556a1
euClaudioFilho <claudiosrfilho@outlook.com>
2024-12-12 14:32:37 -0300

    FEAT: Implementação do mapa com a localização do usuário e dos estabelecimentos parceiros e melhoria no design do BottomSheet

--- nearby-app/src/app/home.tsx
+++ nearby-app/src/app/home.tsx
@@ -5,9 +5,18 @@ import { Categories } from "@/components/categories";
 import { CategoriesProps } from "@/components/categories";
 import { PlaceProps } from "@/components/place";
 import { Places } from "@/components/places";
+import MapView, { Callout, Marker } from "react-native-maps"
+import * as Location from "expo-location"
+import { colors, fontFamily } from "@/styles/theme"
+import { router } from "expo-router";
 
 type MarketsProps = PlaceProps & {
-
+    latitude: number
+    longitude: number
+}
+const currentLocation = {
+    latitude: -23.561187293883442,
+    longitude: -46.656451388116494
 }
 
 export default function Home() {
@@ -41,6 +50,21 @@ export default function Home() {
 
     }
 
+    async function getCurrentLocation() {
+        try {
+            const { granted } = await Location.requestForegroundPermissionsAsync()
+
+            if (granted) {
+                const location = await Location.getCurrentPositionAsync()
+                console.log(location);
+            }
+
+        } catch (error) {
+            console.log(error);
+        }
+
+    }
+
     useEffect(() => {
         fetchCategories()
     }, [])
@@ -52,6 +76,65 @@ export default function Home() {
     return (
         <View style={{ flex: 1 }}>
             <Categories data={categories} onSelect={setCategory} selected={category} />
+
+            <MapView
+                style={{ flex: 1 }}
+                initialRegion={{
+                    latitude: currentLocation.latitude,
+                    longitude: currentLocation.longitude,
+                    latitudeDelta: 0.01,
+                    longitudeDelta: 0.01
+                }}
+            >
+                <Marker
+                    identifier="current"
+                    coordinate={{
+                        latitude: currentLocation.latitude,
+                        longitude: currentLocation.longitude
+                    }}
+                    image={require("@/assets/location.png")}
+                />
+
+                {
+                    markets.map((item) => (
+                        <Marker
+                            key={item.id}
+                            identifier={item.id}
+                            coordinate={{
+                                latitude: item.latitude,
+                                longitude: item.longitude
+                            }}
+                            image={require("@/assets/pin.png")}
+                        >
+                            <Callout onPress={() => router.navigate(`/market/${item.id}`)}>
+                                <View>
+                                    <Text
+                                        style={{
+                                            fontSize: 14,
+                                            color: colors.gray[600],
+                                            fontFamily:
+                                                fontFamily.medium
+                                        }}
+                                    >
+                                        {item.name}
+                                    </Text>
+
+                                    <Text
+                                        style={{
+                                            fontSize: 12,
+                                            color: colors.gray[600],
+                                            fontFamily:
+                                                fontFamily.regular
+                                        }}
+                                    >
+                                        {item.address}
+                                    </Text>
+                                </View>
+                            </Callout>
+                        </Marker>
+                    ))
+                }
+            </MapView>
 
             <Places data={markets} />
         </View>
